659a8ae648f7bc7f45f0c23939ce6c813d1d8b74
James Stamps Jr <jstampshere@gmail.com>
2020-03-08 21:33:26 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ function init() {
   let index = 0;
   document.body.addEventListener("keydown", (e) => {
    
-   
+   function onKeyDownHandler(e) {
      const key = e.key;
      
       index = (key === codes[index]) ? ++index : 0;
@@ -25,7 +25,7 @@ function init() {
             window.alert("Hurray!");
               index = 0; 
      }
-   
+   }
   });
 }
 
